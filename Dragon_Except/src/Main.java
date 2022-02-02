b72529ac904f4d5168609f1e17bc83524711e19b
aawilson100@gmail.com <aawilson100@gmail.com>
2022-02-02 17:36:22 -0500

    Project 3 Exceptions
    Dragon Cave and Number Guess with Exceptions

---
+++ Dragon_Except/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+
+public class Main {
+        public static int choice;
+        public static Scanner input;
+        public static void main(String[] args) {
+            int flip = (int) ( Math.random() * 2 + 1);
+
+            System.out.println("You are in a land full of dragons. In front of you,");
+            System.out.println("you see two caves. In one cave, the dragon is friendly");
+            System.out.println("and will share his treasure with you. The other dragon");
+            System.out.println("is greedy and hungry and will eat you on sight.");
+            System.out.println("Which cave will you go into? (1 or 2)");
+            //reads user input
+            coinflip();
+            System.out.println("You approach the cave...");
+            System.out.println("It is dark and spooky...");
+            System.out.println("A large dragon jumps out in front of you! He opens his jaws and...");
+
+            //if user choice matches randomly decided input, receives good ending, otherwise you get BIT
+            if (choice == flip) {
+                System.out.println("Reveals the Treasure hidden under it's tongue! Congratulations");
+            } else if (choice > 2 | choice < 1) {
+                System.out.println("Invalid entry - please enter 1 or 2: ");
+                flip = input.nextInt();
+            }else{
+                System.out.println("Gobbles you down in one bite!");
+            }
+        }
+        public static void coinflip(){
+
+            try{
+                input = new Scanner(System.in);
+                choice = input.nextInt();
+            }catch(Exception e){
+                System.out.println("Invalid Option, Try again");
+                coinflip();
+            }
+
+
+
+        }
+    }
+
+
---
+++ Guess_Except/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+    public class Main {
+        //running the game
+        public static void randomNum(){
+            //variable initialization
+            String name;
+            int min = 1;
+            int max = 20;
+            int number = 0;
+            int guesses = 0;
+            Scanner input = new Scanner(System.in);
+            int secret = (int) (Math.random()*(max-min)) + min;
+
+            //beginning prompt to print input name
+            System.out.println("Hello! What is your name?");
+            name = input.nextLine();
+            System.out.println("well, " + name +  ", I am thinking of a number between 1 and 20.");
+
+            //keep guessing till you guess it RIGHT
+            while(number!=secret) {
+                System.out.println("Take a guess.");
+                input = new Scanner(System.in);
+                number = input.nextInt();
+                if(number > secret) {
+                    System.out.println("Your guess is too high.");
+                } else if (number < secret) {
+                    System.out.println("Your guess is too low.");
+                }
+                guesses++;
+            }
+
+            //different prompts for guessing on first try, or after multiple attempts
+            if(guesses == 1) {
+                System.out.println("Good Job, " + name + "! You guessed my number on the 1st try!");
+            } else {
+                System.out.println("Good Job, " + name + "! You guessed my number in "+ guesses + " guesses!");
+            }
+            //calls play again to decide if randomNum will run again or stop running
+            playAgain();
+        }
+
+        //running the prompt to restart the game if the player inputs "Yes" condition
+        public static void playAgain(){
+            Scanner input = new Scanner(System.in);
+            System.out.print("Do you want to play again? (y or n) ");
+            String answer = input.next();
+            //determines if the game restarts, otherwise stop the program
+            if(answer.charAt(0) == 'y' || answer.charAt(0) == 'Y') {
+                randomNum();
+            }else {
+                System.out.print("Thanks for playing! ");
+            }
+        }
+
+
+        public static void main(String[] args) {
+            randomNum();
+        }
+
+
+    }
+
+
